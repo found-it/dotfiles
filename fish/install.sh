401701b68d3a5fd75838451be2c391c3768e3df7
James Petersen <jpetersenames@gmail.com>
2024-01-15 12:52:46 -0700

    disable fish greeting
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- fish/install.sh
+++ fish/install.sh
@@ -8,4 +8,13 @@ mkdir -p "${FISH_CONFIG_DIR}"
 
 ln -s "${PWD}/config.fish" "${FISH_CONFIG_DIR}/config.fish"
 
+SRC_FUNCTIONS_DIR="${PWD}/functions"
+DST_FUNCTIONS_DIR="${FISH_CONFIG_DIR}/functions"
+mkdir -p "${DST_FUNCTIONS_DIR}"
+
+for filepath in ${SRC_FUNCTIONS_DIR}/*; do
+  filename=$(basename "${filepath}")
+  ln -s "${SRC_FUNCTIONS_DIR}/${filename}" "${DST_FUNCTIONS_DIR}/${filename}"
+done
+
 printf "[fish] done installing\n"
