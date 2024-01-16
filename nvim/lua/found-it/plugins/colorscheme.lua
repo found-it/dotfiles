2c59548656fc24653a0c81ce034620532f27465e
James Petersen <jpetersenames@gmail.com>
2024-01-15 21:34:53 -0700

    more nvim config
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- nvim/lua/found-it/plugins/colorscheme.lua
+++ nvim/lua/found-it/plugins/colorscheme.lua
@@ -7,6 +7,17 @@ return {
         catppuccin.setup({
             flavour = "mocha", -- latte, frappe, macchiato, mocha
             transparent_background = true,
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                notify = false,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
+                },
+            }
         })
         vim.cmd.colorscheme "catppuccin"
     end,
